ac113f47806df8d0b629040f02b7024e2f903828
Aman-Kasaudhan <rajg99842@gmail.com>
2026-07-09 14:47:57 +0530

    Update CORS configuration for Socket.IO

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,8 @@ const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: [
-            // process.env.CLIENT_URL,
-            "https://secureshare-alpha.vercel.app/"
+            process.env.CLIENT_URL,
+            // "https://secureshare-alpha.vercel.app"
             "http://localhost:5173"
         ],
         methods: ["GET", "POST"],
